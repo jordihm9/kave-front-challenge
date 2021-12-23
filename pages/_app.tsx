1e0d5fcbda5195d0c35e5e3854246dd3ede50eb6
Jordi Hernandez <jjordihm9@gmail.com>
2021-12-23 19:48:50 +0100

    customized Custom  to load Poppins font for all pages, set description, title...

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
+import 'styles/globals.css';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Kave Home Challenge</title>
+        <meta name="description" content="Kave Home" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,8 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Kave Home Challenge</title>
-        <meta name="description" content="Kave Home" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>
         <h1>Hello Kave Home!</h1>
       </main>
