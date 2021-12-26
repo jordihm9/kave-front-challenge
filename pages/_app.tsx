0e2d8dcee670ac6e6029e9ee74c56e21d37c0635
Jordi Hernandez <jjordihm9@gmail.com>
2021-12-26 21:17:42 +0100

    added noto serif font + changed black color theme and fixed typo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&family=Poppins:wght@400;500;700&display=swap" rel="stylesheet"/>
       </Head>
       <Component {...pageProps} />
     </>
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,7 +1,7 @@
 export enum COLORS {
   White = '#FFFFFF',
-  Black = '#000000',
-  Hrey = '#505050',
+  Black = '#4A4A4A',
+  Grey = '#505050',
   SoftGrey = '#F5F5F5',
   Primary = '#FF3D3A'
 }
